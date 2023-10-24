5bc61cf1deb9df62984b7cf532d67a882ee00f33
deepss1 <deepshah003@gmail.com>
2023-10-24 11:05:11 +0530

    Fix notification for MacOS Ventura
    Use UNMutableNotificationContent for newer macos versions

--- macos/Classes/LocalNotifierPlugin.swift
+++ macos/Classes/LocalNotifierPlugin.swift
@@ -1,7 +1,8 @@
 import Cocoa
 import FlutterMacOS
+import UserNotifications
 
-public class LocalNotifierPlugin: NSObject, FlutterPlugin, NSUserNotificationCenterDelegate {
+public class LocalNotifierPlugin: NSObject, FlutterPlugin, NSUserNotificationCenterDelegate, UNUserNotificationCenterDelegate {
     var registrar: FlutterPluginRegistrar!;
     var channel: FlutterMethodChannel!
     
@@ -9,7 +10,11 @@ public class LocalNotifierPlugin: NSObject, FlutterPlugin, NSUserNotificationCen
     
     public override init() {
         super.init()
-        NSUserNotificationCenter.default.delegate = self
+        if #available(macOS 10.14, *) {
+            UNUserNotificationCenter.current().delegate = self
+        } else {
+            NSUserNotificationCenter.default.delegate = self
+        }
     }
     
     public static func register(with registrar: FlutterPluginRegistrar) {
@@ -36,27 +41,59 @@ public class LocalNotifierPlugin: NSObject, FlutterPlugin, NSUserNotificationCen
     public func notify(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         let args = call.arguments as! Dictionary<String, Any>
         let identifier: String = args["identifier"] as! String
-        let title: String? = args["title"] as? String
-        let subtitle: String? = args["subtitle"] as? String
-        let body: String? = args["body"] as? String
+        let title: String = args["title"] as! String
+        let body: String = args["body"] as! String
         
-        let notification = NSUserNotification()
-        notification.identifier = identifier
-        notification.title = title
-        notification.subtitle = subtitle
-        notification.informativeText = body
-        notification.soundName = NSUserNotificationDefaultSoundName
-        
-        let actions: [NSDictionary]? = args["actions"] as? [NSDictionary];
-        
-        if (actions != nil && !(actions!.isEmpty)) {
-            let actionDict =  actions!.first as! [String: Any]
-            let actionText: String? = actionDict["text"] as? String
-            notification.actionButtonTitle = actionText!
+        if #available(macOS 10.14, *) {
+            let notification = UNMutableNotificationContent()
+            notification.title = title
+            notification.body = body
+            notification.sound = UNNotificationSound.default
+            
+            let notificationCenter = UNUserNotificationCenter.current()
+            
+            let actions: [NSDictionary]? = args["actions"] as? [NSDictionary];
+            
+            if (actions != nil && !(actions!.isEmpty)) {
+                let actionDict =  actions!.first as! [String: Any]
+                let actionText: String? = actionDict["text"] as? String
+                if ( actionText != nil ) {
+                    let action = UNNotificationAction(identifier: "action_identifier", title: actionText!, options: [])
+                    
+                    let category = UNNotificationCategory(identifier: "category_identifier", actions: [action], intentIdentifiers: [])
+                    
+                    notification.categoryIdentifier = "category_identifier"
+                    notificationCenter.setNotificationCategories([category])
+                }
+            }
+            
+            let request = UNNotificationRequest(identifier: identifier, content: notification, trigger: nil)
+            
+            notificationCenter.add(request) { error in
+              if (error != nil) {
+                print("quick_notify error: \(error!)")
+              }
+            }
+        } else {
+            let subtitle: String? = args["subtitle"] as? String
+            let notification = NSUserNotification()
+            notification.title = title
+            notification.informativeText = body
+            notification.identifier = identifier
+            notification.subtitle = subtitle
+            notification.soundName = NSUserNotificationDefaultSoundName
+            notification.contentImage = NSImage(named: NSImage.Name("AppIcon"))
+            
+            let actions: [NSDictionary]? = args["actions"] as? [NSDictionary];
+            
+            if (actions != nil && !(actions!.isEmpty)) {
+                let actionDict =  actions!.first as! [String: Any]
+                let actionText: String? = actionDict["text"] as? String
+                notification.actionButtonTitle = actionText!
+            }
+            
+            NSUserNotificationCenter.default.deliver(notification)
         }
-        
-        NSUserNotificationCenter.default.deliver(notification)
-        self.notificationDict[identifier] = notification
         
         result(true)
     }
@@ -95,6 +132,27 @@ public class LocalNotifierPlugin: NSObject, FlutterPlugin, NSUserNotificationCen
     
     public func userNotificationCenter(_ center: NSUserNotificationCenter, shouldPresent notification: NSUserNotification) -> Bool {
         return true
+    }
+    
+    public func userNotificationCenter(_ center: UNUserNotificationCenter,
+           didReceive response: UNNotificationResponse,
+           withCompletionHandler completionHandler:
+             @escaping () -> Void) {
+        
+       // Perform the task associated with the action.
+       switch response.actionIdentifier {
+       case "action_identifier":
+           _invokeMethod("onLocalNotificationClickAction", response.notification.request.identifier)
+          break
+            
+       // Handle other actions…
+     
+       default:
+          break
+       }
+        
+       // Always call the completion handler when done.
+       completionHandler()
     }
     
     public func _invokeMethod(_ methodName: String, _ notificationId: String) {
